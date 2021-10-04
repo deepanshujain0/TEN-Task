c26972be20e3672776957e0f61f224232542dfb9
Deepanshu Jain <85442024+deepanshujain0@users.noreply.github.com>
2021-10-04 08:47:57 +0530

    Add files via upload

---
+++ Emoji Search - Task#4/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchBar from './SearchBar';
+function App() {
+  
+return (
+  <div classname='App'>
+       <SearchBar/>
+   </div>
+   
+)
+}
+
+export default App;
